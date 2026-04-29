aa61d5a1f3f1cc5fa7427d4cf0b5e8659155514a
Anuj mehra <mehraanuj2018@gmail.com>
2026-04-29 20:37:59 +0530

    three dot added

--- app/page.tsx
+++ app/page.tsx
@@ -176,12 +176,21 @@ export default function Home() {
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path></svg>
           </button>
 
-          {/* Hamburger Menu Icon */}
+          {/* Hamburger Menu Icon (Mobile) */}
           <button 
             onClick={() => setIsMenuOpen(true)}
             className="md:hidden p-2 rounded-lg transition" style={{ color: "#f5f0e8" }}
           >
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+          </button>
+
+          {/* Three Dot Menu Icon (Desktop) */}
+          <button 
+            onClick={() => setIsMenuOpen(true)}
+            className="hidden md:block p-2 rounded-lg transition hover:bg-white/10" style={{ color: "#f5f0e8" }}
+            title="Open Menu"
+          >
+            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"></path></svg>
           </button>
         </div>
       </header>
